de387c1263791e3d6c0c5fbcce0e3d06b42fe3e9
prithvimangya <prithvimangyal@gmail.com>
2024-12-10 16:03:33 +0530

    Create simple_intrest.py

---
+++ simple_intrest.py
@@ -0,0 +1,5 @@
+p = float(input("principle amount")) # Convert input to float
+n = float(input("number of years"))   # Convert input to float
+r = float(input("rata of interest"))
+si = (p * n * r) / 100
+print(si)
