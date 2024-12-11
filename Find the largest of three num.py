9fc99c0224060d8418ec4e4d4ce29dd21617e6da
prithvimangya <prithvimangyal@gmail.com>
2024-12-11 14:06:54 +0530

    Create Find the largest of three num.py

---
+++ Find the largest of three num.py
@@ -0,0 +1,9 @@
+num1 = float(input("Enter first number:"))
+num2 = float(input("Enter second number:"))
+num3 = float(input("Enter third number:"))
+if num1>=num2 and num1>=num3:
+           print("Largest number is:", num1)
+elif num2>=num3:
+            print("Largest number is:", num2)
+else:
+            print("Largest number is:", num3)
